dd75eb401a2dec1fcf1e2eb614de1112d8d6cc73
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2025-08-11 13:54:36 +0530

    redux persists

--- lib/redux/provider.tsx
+++ lib/redux/provider.tsx
@@ -3,7 +3,18 @@
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { ReactNode } from 'react'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+// Create persistor
+const persistor = persistStore(store);
 
 export function ReduxProvider({ children }: { children: ReactNode }) {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 } 
--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,4 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import galleryReducer from "./features/gallerySlice";
 import authReducer from "./features/authSlice";
 import blogReducer from "./features/blogSlice";
@@ -12,24 +14,42 @@ import vendorReducer from "./features/vendorSlice";
 import userReducer from "./features/userSlice";
 import weddingReducer from "./features/inviteSlice";
 
+// Configuration for redux-persist
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['auth'] // only auth will be persisted
+};
+
+const rootReducer = combineReducers({
+  gallery: galleryReducer,
+  auth: authReducer,
+  blog: blogReducer,
+  course: courseReducer,
+  subscriber: subscriberReducer,
+  contact: contactReducer,
+  test: testReducer,
+  hotel: hotelReducer,
+  admin: adminReducer,
+  vendor: vendorReducer,
+  user: userReducer,
+  wedding: weddingReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    gallery: galleryReducer,
-    auth: authReducer,
-    blog: blogReducer,
-    course: courseReducer,
-    subscriber: subscriberReducer,
-    contact: contactReducer,
-    test: testReducer,
-    hotel: hotelReducer,
-    admin: adminReducer,
-    vendor: vendorReducer,
-    user: userReducer,
-    wedding: weddingReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/REGISTER'],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
+export const persistor = persistStore(store);
 export default store;
